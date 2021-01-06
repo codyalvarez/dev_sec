0aa170d5f1a0bed80451f31074f82994f921a885
codyalvarez <jedi.alvarez805@gmail.com>
2021-01-05 21:24:39 -0500

    update

--- dev_sec.gemspec
+++ dev_sec.gemspec
@@ -6,17 +6,17 @@ Gem::Specification.new do |spec|
   spec.authors       = ["cody alvarez"]
   spec.email         = ["codemk13@outlook.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "i dont need you to tell me how good my coffee is"
+  spec.description   = "some info"
+  spec.homepage      = "https://github.com/codyalvarez/dev_sec"
   spec.license       = "MIT"
   spec.required_ruby_version = Gem::Requirement.new(">= 2.3.0")
 
   spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/codyalvarez/dev_sec"
+  # spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
--- lib/dev_sec/cli.rb
+++ lib/dev_sec/cli.rb
@@ -1,56 +1,17 @@
 class DevSec::CLI 
   def call
     intro
-    end 
-
-  def intro
-    puts "We've been waiting for you"
-    puts "You don't know us, but we know you."
-    puts "We've been waiting for you."
-    puts "There's not much time though."
-    puts "A simple test to get things moving"
-    unlock
   end
-
-  def sleep_counter
-    sleep(1.0)
-    puts "."
-    sleep(1.0)
-    puts ".."
-    sleep(1.0)
-    puts "..."
-    sleep(1.0)
-  end
-
-  def unlock
-    sleep_counter
-    cipher_one
-  end
-
-  def user_input
-    user_input = gets.strip
-  end
-
-  def cipher_one
-    puts "~ crack the code ~"
-    puts "5-1-19-25-15-14-5"
-    if user_input == "EASYONE"
-      puts "Permission granted."
-      permission_granted
-    else 
-      puts "Permission denied, try again."
-      cipher_one
-    end
-  end
-
-  def permission_granted
-    sleep_counter
-    puts "Just wanted to make sure you were the real deal. Ok, lets get to work."
-    sleep_counter
     
+  def intro 
+    puts "Hi! Welcome to Dev Sec, My name is 8Bit, what is your name ?"
+    name = gets.strip
+    greeting(name)
   end
 
-
+  def greeting(name)
+    puts "Hi #{name}."
+  end
 end
 
 
@@ -59,4 +20,59 @@ end
 
 
 
-# ./bin/dev_sec
+
+
+
+
+
+
+
+  #   def intro
+#     puts "We've been waiting for you"
+#     puts "You don't know us, but we know you."
+#     puts "We've been waiting for you."
+#     puts "There's not much time though."
+#     puts "A simple test to get things moving"
+#     unlock
+#   end
+
+# #   def sleep_counter
+# #     sleep(0.5)
+# #     puts "."
+# #     sleep(0.5)
+# #     puts ".."
+# #     sleep(0.5)
+# #     puts "..."
+# #     sleep(0.5)
+# #   end
+
+# #   def unlock
+# #     sleep_counter
+# #     cipher_one
+# #   end
+
+# #   def user_input
+# #     user_input = gets.strip
+# #   end
+
+# #   def cipher_one
+# #     puts "~ crack the code ~"
+# #     puts "5-1-19-25-15-14-5"
+# #     if user_input == "EASYONE"
+# #       puts "Access granted."
+# #       permission_granted
+# #     else 
+# #       puts "Access denied, try again."
+# #       cipher_one
+# #     end
+# #   end
+
+# #   def permission_granted
+# #     sleep_counter
+# #     puts "Just wanted to make sure you were the real deal. Ok, lets get to work."
+# #     sleep_counter
+    
+# #   end
+
+
+# # end
